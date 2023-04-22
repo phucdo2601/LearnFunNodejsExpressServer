fe1360e979228b06f0e5f01754371d3491cc45a6
phucdo2601 <ngocphucdo2601@gmail.com>
2023-04-22 16:33:30 +0700

    First learn create basic nodejs server b01

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const server = http.createServer((req, res) => {
+    console.log('run request...');
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<h1>Hello, world!</h1>');
+    res.write('<h2>From PhucDn</h2>');
+    res.end();
+})
+
+server.listen(3000, 'localhost', ()=>{
+    console.log('Node.JS server is running on port: 3000');
+});
